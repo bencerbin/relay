16bdc97449ace7e48081c83dce1aafa6b1bc3db1
Ben Cerbin <cben2004@gmail.com>
2026-08-12 10:12:57 +0100

    urls

---
+++ referrals/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import RecommendationView
+
+urlpatterns = [
+    path(
+        "recommendations/",
+        RecommendationView.as_view(),
+        name="recommendations",
+    ),
+]
